5bba4ed212ff4125b40ab488e759d27ecafd73d7
Takuya Tsuchida <tsucchi@cpan.org>
2014-05-31 12:42:54 +0900

    color_enable is moved role to class

--- lib/Acme/MilkyHolmes/Role/HasPersonalColor.pm
+++ lib/Acme/MilkyHolmes/Role/HasPersonalColor.pm
@@ -16,20 +16,7 @@ my $ansi_colors = {
 
 sub color_enable {
     my ($self) = shift;
-
-    if ( @_ ) {
-        $self->{color_enable} = $_[0];
-    }
-    else {
-        if ( defined $self->{color_enable} ) {
-            return $self->{color_enable}
-        }
-        if ( defined $self->common->[0]->{color_enable} ) {
-            my $color_enable = $self->common->[0]->{color_enable} + 0;
-            return $color_enable;
-        }
-    }
-    return 1; #default is true
+    return 1;
 }
 
 sub color {
