2e56a2c353b8756911b848633319b4d5663a96b5
Takuya Tsuchida <tsucchi@cpan.org>
2014-06-25 21:47:16 +0900

    support sufficient Kazumi's voiced_by

--- lib/Acme/MilkyHolmes/Character/KazumiTokiwa.pm
+++ lib/Acme/MilkyHolmes/Character/KazumiTokiwa.pm
@@ -3,8 +3,20 @@ use Mouse;
 extends 'Acme::MilkyHolmes::Group::MilkyHolmes';
 
 use utf8;
+use Time::Piece;
 
 no Mouse;
+
+sub voiced_by { #override
+    my ($self) = @_;
+    my $now = localtime();
+    my $threshold = Time::Piece->strptime('2013-12-25T00:00:00Z', '%Y-%m-%dT%H:%M:%SZ');
+    if ( $now >= $threshold ) {
+        return $self->_localized_field('voiced_by');
+    }
+    return $self->_localized_field('voiced_by_before');
+}
+
 
 1;
 
@@ -21,6 +33,7 @@ firstname: Kazumi
 familyname: Tokiwa
 birthday:
 voiced_by: Aimi
+voiced_by_before: Aimi Terakawa
 toys: Arrow
 
 @@ ja
@@ -29,4 +42,5 @@ firstname: カズミ
 familyname: 常盤
 birthday:
 voiced_by: 愛美
+voiced_by_before: 寺川 愛美
 toys: アロー
