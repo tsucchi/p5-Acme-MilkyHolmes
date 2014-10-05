b663a1e40cfc193541965ec360e42b8053594099
Takuya Tsuchida <tsucchi@cpan.org>
2014-10-05 23:54:34 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 55cedb2..9204497 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for Perl extension Acme-MilkyHolmes
    
     {{$NEXT}}
    
    +0.04 2014-10-05T14:53:43Z
    +
         - new feature: nickname_voiced_by() (htk293)
    
     0.03 2014-06-25T14:18:04Z

--- lib/Acme/MilkyHolmes.pm
+++ lib/Acme/MilkyHolmes.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use parent qw(Exporter);
 
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 
 use Acme::MilkyHolmes::Character::SherlockShellingford;
 use Acme::MilkyHolmes::Character::NeroYuzurizaki;
