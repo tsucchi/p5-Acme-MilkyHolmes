c9dd8cad783882a7af316fe8f257cacb63d1f01f
Takuya Tsuchida <tsucchi@cpan.org>
2015-11-20 00:01:41 +0900

    Checking in changes prior to tagging of version 0.06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index a7a304d..3210e14 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Revision history for Perl extension Acme-MilkyHolmes
    
     {{$NEXT}}
    +
    +0.06 2015-11-19T15:00:18Z
         - Happy Birthday Kazumi!!(add Kazumi's birthday)

--- lib/Acme/MilkyHolmes.pm
+++ lib/Acme/MilkyHolmes.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use parent qw(Exporter);
 
-our $VERSION = "0.05";
+our $VERSION = "0.06";
 
 use Acme::MilkyHolmes::Character::SherlockShellingford;
 use Acme::MilkyHolmes::Character::NeroYuzurizaki;
